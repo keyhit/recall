4eeb11c5fc2f23a014132ec9defe2a4fb117d3c4
keyhit <viacheslavkarpenkoju@gmail.com>
2018-11-28 12:57:15 +0200

    added parts scaffold refactored routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :projects
-  resources :stages
-  resources :comments
-  resources :tasks
+  resources :users do 
+    resources :projects do
+      resources :parts do
+        resources :tasks do
+          resources :comments
+        end
+      end
+    end
+  end
   resources :components
   get 'session/login', to: 'sessions#login', as: 'login'
   post 'session/create', to: 'sessions#create', as: 'create_session'
   get 'session/logout', to: 'sessions#logout', as: 'loguot'
 
-  root 'users#new'
+  root 'sessions#login'
+      # resources :stages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
